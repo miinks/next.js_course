b297a11516f70b56997496f2b0f769462e980e20
Regie Agustin <124678791+miinks@users.noreply.github.com>
2024-09-14 05:34:25 -0500

    save

--- hello-world/src/app/page.tsx
+++ hello-world/src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Save</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
